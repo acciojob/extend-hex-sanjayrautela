35a0ab3bcd83fa84a7a63fce354c467bd637dad0
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-09 11:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ const extendHex = (shortHex) => {
     const r = shortHex.charAt(0);
 	const g = shortHex.charAt(1);
 	const b = shortHex.charAt(2);
+	
 
 	const fullHex = '#${r}${r}${g}${g}${b}${b}';
 
 	return fullHex.toUpperCase();
 };
 
-Do not change the code below.
+//Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
 
