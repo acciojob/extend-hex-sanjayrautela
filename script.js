04dd30292bdc5cb9d3b870a880b9e516e60fb0ef
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-09 11:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  if(shortHex.charAt(0) === '#'){
+	  shortHex = shortHex.substring(1);
+  }
+
+    const r = shortHex.charAt(0);
+	const g = shortHex.charAt(1);
+	const b = shortHex.charAt(2);
+
+	const fullHex = '#${r}${r}${g}${g}${b}${b}';
+
+	return fullHex.toUpperCase();
+
+	
+	
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
